1a9eee1c2c44289e8c6fd17906ca835e1505fd75
varun <varunss28299@gmail.com>
2026-06-28 00:35:46 +0530

    Shop and Product page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
 import Shop from './components/Shop'
+import Product from './components/Product'
+
+
 
 function App() {
 
@@ -11,6 +14,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/shop' element={<Shop />} />
+        <Route path='/product' element={<Product />} />
       </Routes>
     </BrowserRouter>
      
